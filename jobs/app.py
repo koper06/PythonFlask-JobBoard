9ded88b1bb42e08d9fc491b6cd1724db8837b3fc
Maor Kuperman <maor.kuperman@brightmachines.com>
2020-03-23 17:07:21 +0200

    app connection

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,17 @@
 import flask
-from flask import render_template, Flask
+from flask import render_template, Flask, g
+import sqlite3
 
+PATH = 'db\jobs.sqlite'
 app = flask.Flask(__name__)
 
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection, g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.row
+    return connection
+    
 @app.route('/')
 @app.route('/jobs')
 def jobs():
