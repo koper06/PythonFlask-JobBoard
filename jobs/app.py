151670dbf0fc04c99aa0fca295355e3fe4f72bd1
Maor Kuperman <maor.kuperman@brightmachines.com>
2020-03-23 17:20:49 +0200

    as

--- jobs/app.py
+++ jobs/app.py
@@ -11,7 +11,18 @@ def open_connection():
         connection, g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.row
     return connection
-    
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor=connection.execute(sql,values)
+    if commit:
+        results=connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+
 @app.route('/')
 @app.route('/jobs')
 def jobs():
