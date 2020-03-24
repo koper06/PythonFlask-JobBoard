e27759b2501eea7bac84331d0063deba561348ae
Maor Kuperman <maor.kuperman@brightmachines.com>
2020-03-24 15:41:08 +0200

    2

--- jobs/app.py
+++ jobs/app.py
@@ -7,7 +7,7 @@ app = flask.Flask(__name__)
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        connection, g._connection = sqlite3.connect(PATH)
+        connection, g._connection = sqlite3.Connect(PATH)
     connection.row_factory = sqlite3.row
     return connection
 
